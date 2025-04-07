9036f99a99537d3ea573d6daab53e78856df4c64
Alberto Ayuso <alberto.ayuso@vasscompany.com>
2025-04-07 21:56:02 +0200

    CodingKey on CharacterModel
    Apply a layer of security on model in case empty data arrives

--- AyusoConMorty/Characters/CharacterModel.swift
+++ AyusoConMorty/Characters/CharacterModel.swift
@@ -15,6 +15,21 @@ struct CharacterModel: Codable, Identifiable, Equatable, Hashable {
     let species: String
     let gender: String
     let location: CharacterLocationModel
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, image, status, species, gender, location
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(Int.self, forKey: .id)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.image = try container.decode(String.self, forKey: .image)
+        self.status = try container.decode(String.self, forKey: .status)
+        self.species = try container.decode(String.self, forKey: .species)
+        self.gender = try container.decode(String.self, forKey: .gender)
+        self.location = try container.decodeIfPresent(CharacterLocationModel.self, forKey: .location) ?? CharacterLocationModel(name: "unknown")
+    }
 }
 
 struct CharacterLocationModel: Codable, Equatable, Hashable{
